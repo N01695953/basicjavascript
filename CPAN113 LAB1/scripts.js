574b6162197f7ccced50db54be6b881a9d8c2b3e
N01695953 <fariahyusuf7@gmail.com>
2025-02-01 15:25:39 -0500

    Add files via upload

---
+++ CPAN113 LAB1/scripts.js
@@ -0,0 +1,58 @@
+//Declare and Use Variables
+let studentName = "Fariah"
+let studentAge = "10"
+let isEnrolled = "true"
+
+console.log("Student Name :", studentName)
+console.log("Student Age :", studentAge)
+console.log("Enrollment :", isEnrolled)
+
+//Demonstrate Data Types
+//String
+let programName = "Computer Programming"
+//Number
+let courseNumber = "113"
+//Boolean
+let ECE = "false"
+//Array
+let fruits = ["apple", "banana", "cherry"];
+//Object
+let student = {
+    Name : "Fariah Yusuf",
+    studentNumber : "n01695953"
+};
+
+console.log("Program Name :", programName)
+console.log("Course Number :", courseNumber)
+console.log("ECE :", ECE)
+console.log("Fruits :", fruits[0])
+console.log("Fruits :", fruits[1])
+console.log("Fruits :", fruits[2])
+console.log("Fruits :", fruits)
+console.log("student :", student.Name)
+console.log("student :", student.studentNumber)
+
+//Use Basic Operators
+//Arithmetic Operators
+let a = 12;
+let b = 2;
+
+console.log("Addition :", a+b)
+console.log("Subtraction :", a-b)
+console.log("Multiplication :", a*b)
+console.log("Division :", a/b)
+//Comparison Operators
+console.log("Greater than :", a>b)
+console.log("Less than :", a<b)
+console.log("Equal to :", a===b)
+console.log("Not equal :", a!==b)
+
+//Logical Operators
+let x = false;
+let y = true;
+
+console.log("AND :", x && y)
+console.log("OR :", x || y)
+console.log("NOT :", !x)
+console.log("NOT :", !y)
+
